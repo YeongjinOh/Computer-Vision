d9177975a7a85c9a1b25e71bd244f5430b9d7058
YeongjinOh <oyj9109@snu.ac.kr>
2016-05-30 00:55:42 +0900

    Convolutional neural network - cnnConvolve

--- Convolutional_neural_network/src/cnnConvolve.m
+++ Convolutional_neural_network/src/cnnConvolve.m
@@ -15,16 +15,22 @@ function convolvedFeatures = cnnConvolve(patchDim, numFeatures, images, W, b, ZC
 %  convolvedFeatures - matrix of convolved features in the form
 %                      convolvedFeatures(featureNum, imageNum, imageRow, imageCol)
 
+numImages = size(images, 4);
+imageDim = size(images, 1);
+imageChannels = size(images, 3);
+
+%convolvedFeatures = zeros(numFeatures, numImages, imageDim - patchDim + 1, imageDim - patchDim + 1);
+
 % Instructions:
-%   Convolve every feature with every large image here to produce the 
-%   numFeatures x numImages x (imageDim - patchDim + 1) x (imageDim - patchDim + 1) 
-%   matrix convolvedFeatures, such that 
+%   Convolve every feature with every large image here to produce the
+%   numFeatures x numImages x (imageDim - patchDim + 1) x (imageDim - patchDim + 1)
+%   matrix convolvedFeatures, such that
 %   convolvedFeatures(featureNum, imageNum, imageRow, imageCol) is the
 %   value of the convolved featureNum feature for the imageNum image over
 %   the region (imageRow, imageCol) to (imageRow + patchDim - 1, imageCol + patchDim - 1)
 %
-% Expected running times: 
-%   Convolving with 100 images should take less than 3 minutes 
+% Expected running times:
+%   Convolving with 100 images should take less than 3 minutes
 %   Convolving with 5000 images should take around an hour
 %   (So to save time when testing, you should convolve with less images, as
 %   described earlier)
@@ -34,56 +40,64 @@ function convolvedFeatures = cnnConvolve(patchDim, numFeatures, images, W, b, ZC
 % steps
 
 
+wt = W * ZCAWhite;
+bt = b - wt * meanPatch;
+
 
 
 % --------------------------------------------------------
 
 convolvedFeatures = zeros(numFeatures, numImages, imageDim - patchDim + 1, imageDim - patchDim + 1);
 for imageNum = 1:numImages
-  for featureNum = 1:numFeatures
+    for featureNum = 1:numFeatures
+        
+        % convolution of image with feature matrix for each channel
+        convolvedImage = zeros(imageDim - patchDim + 1, imageDim - patchDim + 1);
+        for channel = 1:3
+            
+            % Obtain the feature (patchDim x patchDim) needed during the convolution
+            % ---- YOUR CODE HERE ----
+            
+            offset = patchDim*patchDim*(channel-1);
+            feature = reshape(wt(featureNum, offset+1 : offset+patchDim*patchDim), patchDim, patchDim);
+            
+            
+            
+            
+            % ------------------------
+            
+            % Flip the feature matrix because of the definition of convolution, as explained later
+            feature = flipud(fliplr(squeeze(feature)));
+            
+            % Obtain the image
+            im = squeeze(images(:, :, channel, imageNum));
+            
+            % Convolve "feature" with "im", adding the result to convolvedImage
+            % be sure to do a 'valid' convolution
+            % ---- YOUR CODE HERE ----
 
-    % convolution of image with feature matrix for each channel
-    convolvedImage = zeros(imageDim - patchDim + 1, imageDim - patchDim + 1);
-    for channel = 1:3
+            convolvedImage= convolvedImage + conv2(im, feature, 'valid');
 
-      % Obtain the feature (patchDim x patchDim) needed during the convolution
-      % ---- YOUR CODE HERE ----
-      feature = zeros(8,8); % You should replace this
-      
-      
-      
-      
-      % ------------------------
-
-      % Flip the feature matrix because of the definition of convolution, as explained later
-      feature = flipud(fliplr(squeeze(feature)));
-      
-      % Obtain the image
-      im = squeeze(images(:, :, channel, imageNum));
-
-      % Convolve "feature" with "im", adding the result to convolvedImage
-      % be sure to do a 'valid' convolution
-      % ---- YOUR CODE HERE ----
-
-      
-      
-      
-      % ------------------------
-
+            
+            
+            
+            % ------------------------
+            
+        end
+        
+        % Subtract the bias unit (correcting for the mean subtraction as well)
+        % Then, apply the sigmoid function to get the hidden activation
+        % ---- YOUR CODE HERE ----
+        
+        convolvedImage = convolvedImage + bt(featureNum);
+        convolvedImage = 1.0 ./ ( 1.0 + exp(-convolvedImage));
+        
+        
+        % ------------------------
+        
+        % The convolved feature is the sum of the convolved values for all channels
+        convolvedFeatures(featureNum, imageNum, :, :) = convolvedImage;
     end
-    
-    % Subtract the bias unit (correcting for the mean subtraction as well)
-    % Then, apply the sigmoid function to get the hidden activation
-    % ---- YOUR CODE HERE ----
-
-    
-    
-    
-    % ------------------------
-    
-    % The convolved feature is the sum of the convolved values for all channels
-    convolvedFeatures(featureNum, imageNum, :, :) = convolvedImage;
-  end
 end
 
 
